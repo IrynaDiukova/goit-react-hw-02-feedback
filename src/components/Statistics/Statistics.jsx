67c5774c06aefecdebdb170698a28915b1071106
Iryna Diukova <diukovairyna@ukr.net>
2023-01-03 16:39:24 +0200

    виправила

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from './Statistics.module.css';
 
 
 
-const Statistics = ({ title, good, neutral, bad, total, positivePercentage }) => {
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
     
     return (
         <ul className={css.statistics}>
